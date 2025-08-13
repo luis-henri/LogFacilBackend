23d4aa6b9b6526ed88376142101a49dee9bcca73
Luis Henrique <lhgmatias@gmail.com>
2025-08-13 08:40:00 -0300

    atualizando as rotas de busca do usuario

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { login } from '../controllers/authController';
+import { createUser } from '../controllers/userController';
 
 const router = Router();
 
 router.post('/login', login);
+router.post('/register', createUser);
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAllUsers, updateUser } from '../controllers/userController';
+import { getAllUsers, updateUser, getAllPerfis } from '../controllers/userController';
 
 const router = Router();
 
 router.get('/', getAllUsers);
 router.patch('/:id', updateUser);
+router.get('/perfis', getAllPerfis);
 
 export default router;
